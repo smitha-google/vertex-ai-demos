bdaa27b5974114dcd4d568811efdcc70c14f1d85
smitha-google <102045161+smitha-google@users.noreply.github.com>
2024-01-23 13:16:44 -0500

    Intro to gemini using node.js sdk

---
+++ node.js-samples/introtogeminiapi.js
@@ -0,0 +1,40 @@
+const {VertexAI} = require('@google-cloud/vertexai');
+
+// Initialize Vertex with your Cloud project and location
+const vertex_ai = new VertexAI({project: 'smithaargolisinternal', location: 'northamerica-northeast1'});
+const model = 'gemini-pro';
+
+// Instantiate the models
+const generativeModel = vertex_ai.preview.getGenerativeModel({
+  model: model,
+  generation_config: {
+    "max_output_tokens": 2048,
+    "temperature": 0.9,
+    "top_p": 1
+},
+});
+
+async function generateContent() {
+  const req = {
+    contents: [{role: 'user', parts: [{text: "What is a large language model?"}]}],
+  };
+
+  const streamingResp = await generativeModel.generateContentStream(req);
+
+  // Wait for the response stream to complete
+  const aggregatedResponse = await streamingResp.response;
+
+  // Select the text from the response
+  const fullTextResponse =
+    aggregatedResponse.candidates[0].content.parts[0].text;
+
+  console.log(fullTextResponse);
+
+  //for await (const item of streamingResp.stream) {
+    //process.stdout.write('stream chunk: ' + item);
+  //}
+
+  //process.stdout.write('aggregated response: ' + (await streamingResp.response));
+};
+
+generateContent();
